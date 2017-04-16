cab3d75c3bb4bac421956b19801424899d672f92
wesley <wesley.dbr@live.nl>
2017-04-16 13:55:22 +0200

    Added Movie and Show to items

--- Pathe/items.py
+++ Pathe/items.py
@@ -1,8 +1,25 @@
-import scrapy
+from scrapy import Item, Field
 
 
-class Theater(scrapy.Item):
-    id = scrapy.Field()
-    name = scrapy.Field()
-    city = scrapy.Field()
-    image = scrapy.Field()
+class Theater(Item):
+    id = Field()
+    name = Field()
+    city = Field()
+    image = Field()
+
+class Movie(Item):
+    id = Field()
+    title = Field()
+    description = Field()
+    image = Field()
+
+class Show(Item):
+    theater_id = Field()
+    movie_id = Field()
+    date = Field()
+    start = Field()
+    end = Field()
+    duration = Field()
+    type = Field()
+    url = Field()
+    
